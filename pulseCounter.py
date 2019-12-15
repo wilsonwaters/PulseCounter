5d0d6740bc2802eb12eb4e18587ede0f146018f4
Wilson Waters <git-wilson.waters@alintech.com.au>
2019-12-15 14:56:13 +0800

    Include url posting to pvoutput

--- pulseCounter.py
+++ pulseCounter.py
@@ -2,12 +2,16 @@
 import RPi.GPIO as GPIO
 from threading import Thread, Event
 from urllib import request, parse
+from datetime import datetime
 import time
 import signal
 import sys
+import tempfile
+import os
 
 killEventPulseCounter = Event()
 killEventURLPoster = Event()
+killEventPvoutputPoster = Event()
 
 class PulseCounter(Thread):
     # If using polling mode, set the inteval. This should be based
@@ -22,24 +26,69 @@ class PulseCounter(Thread):
     # current pulse count
     count = 0
 
+    # pulse count at midnight today. Used to calculate daily energy
+    countMidnight = 0
+
     # last state for polling mode
     lastState = 0
 
+    # last time a state change was seen
+    lastStateDate = None
+
+    # How often to flush the current count to disk
+    savePeriod = 600
+
+    # last time state was saved
+    lastSaveDate = None
+
     def getCount(self):
         return self.count
+
+    def getCountToday(self):
+        return self.count-self.countMidnight
 
     def __init__(self, isGPIOInturruptMode):
         Thread.__init__(self)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(17, GPIO.IN)
         self.isGPIOInturruptMode = isGPIOInturruptMode
+        self.recoverState()
 
     def run(self):
-        if (self.isGPIOInturruptMode):
-            self.runGPIOInturrupt()
-        else:
-            self.runGPIOPolling()
+        #if (self.isGPIOInturruptMode):
+        #    self.runGPIOInturrupt()
+        #else:
+        self.runGPIOPolling()
         GPIO.cleanup()
+
+    def checkSaveState(self):
+        if (self.lastSaveDate is None or (datetime.now()-self.lastSaveDate).total_seconds() > self.savePeriod):
+            self.saveState()
+            self.lastSaveDate = datetime.now()
+
+    def saveState(self):
+        print("saving state")
+        try:
+            with open(os.path.join(tempfile.gettempdir(), "pulseCounterCount"), 'w') as tempFile:
+                tempFile.write(str(self.count))
+            with open(os.path.join(tempfile.gettempdir(), "pulseCounterCountMidnight"), 'w') as tempFile:
+                tempFile.write(str(self.countMidnight))
+        except Exception as e:
+            print("error saving state: "+str(e))
+
+    def recoverState(self):
+        print("recovering state")
+        try:
+            with open(os.path.join(tempfile.gettempdir(), "pulseCounterCount"), 'r') as tempFile:
+                self.count = int(tempFile.read())
+            with open(os.path.join(tempfile.gettempdir(), "pulseCounterCountMidnight"), 'r') as tempFile:
+                self.countMidnight = int(tempFile.read())
+            mtime = os.path.getmtime(os.path.join(tempfile.gettempdir(), "pulseCounterCountMidnight"))
+            self.lastStateDate = datetime.fromtimestamp(mtime)
+        except Exception as e:
+            print("error recovering state: "+str(e))
+            self.count = 0;
+            self.countMidnight = 0;
 
     def runGPIOPolling(self):
         while not killEventPulseCounter.isSet():
@@ -50,15 +99,24 @@ class PulseCounter(Thread):
                 if (state == 1):
                     self.count += 1
                     print ("inc {}".format(self.count))
+
+                    # determine if midnight has passes and reset the counter
+                    if (self.lastStateDate is None or datetime.now().day != self.lastStateDate.day):
+                        self.countMidnight = self.count
+                self.lastStateDate = datetime.now()
             killEventPulseCounter.wait(self.pollingInterval)
-        print ("thread exiting")
+            self.checkSaveState()
+        self.saveState()
+        print ("thread PulseCounter exiting")
 
     def runGPIOInturrupt(self):
         #GPIO.add_event_detect(17, GPIO.RISING, callback=self.gpioInturrupt, bouncetime=(int)(self.pollingInterval*1000))
         GPIO.add_event_detect(17, GPIO.RISING, callback=self.gpioInturrupt, bouncetime=300)
         while not killEventPulseCounter.isSet():
             killEventPulseCounter.wait(self.pollingInterval)
-        print ("thread 2 exiting")
+            self.checkSaveState()
+        self.saveState()
+        print ("thread PulseCounter exiting")
 
     def gpioInturrupt(self, channel):
         self.count += 1
@@ -86,12 +144,65 @@ class URLPoster(Thread):
                 self.postCount(count)
                 print("posting {}".format(count))
             killEventURLPoster.wait(self.interval)
-        print ("thread 2 exiting")
+        print ("thread URLPoster exiting")
 
     def postCount(self, count):
-        data = "node=meter&data={{pulsecount:{}}}&apikey=1bf5407b6ffff3de68b37e80808425ed".format(count).encode("ascii")
+        data = "node=meter&data={{pulsecount:{}}}&apikey=xxxxxxxx".format(count).encode("ascii")
         req = request.Request(self.url, data)
         resp = request.urlopen(req)
+
+class PVOutputPoster(Thread):
+    # interval in seconds to wait between sending values to the given URL
+    # PVOutput maximum interval time is 5 mins
+    interval = 300
+
+    # API key form the pvoutput settings page
+    pvoutputAPIKey = "setme"
+    pvoutputSystemId = 123456
+
+    #URL to post to
+    url = "https://pvoutput.org/service/r2/addstatus.jsp"
+
+    # class with a getCount() function
+    counter = None
+
+    # counter and time at last event send
+    lastCount = None
+    lastTime = None
+
+    def __init__(self, counter):
+        Thread.__init__(self)
+        self.counter = counter
+
+    def run(self):
+        while not killEventPvoutputPoster.isSet():
+            if (self.counter is not None):
+                count = self.counter.getCount()
+                now = datetime.now()
+                if (self.lastTime is not None):
+                   secondsSinceLast = (now-self.lastTime).total_seconds()
+                   # pulses are always 1 pulse per wh.
+                   # There are 3600 seconds in an hour, so multiply numPulses by the proportion of the hour that has passed to work out average power over the  interval
+                   averageWatts = (count-self.lastCount)*(3600/secondsSinceLast)
+                   energy = self.counter.getCountToday()
+                   self.postAddStatusAPI(now, energy, averageWatts)
+                   print("posting {}".format(count))
+                self.lastCount = count
+                self.lastTime = now
+            killEventPvoutputPoster.wait(self.interval)
+        print ("thread PVOutputPoster exiting")
+
+    def postAddStatusAPI(self, datetime, energyConsumption, powerConsumption):
+        print ("powerConsumption {}".format(powerConsumption))
+        data = "d={}&t={}&v3={}&v4={}".format(datetime.strftime("%Y%m%d"), datetime.strftime("%H:%M"), energyConsumption, powerConsumption).encode("ascii")
+        print ("post {}".format(data))
+        req = request.Request(self.url, data)
+        req.add_header("X-Pvoutput-Apikey", self.pvoutputAPIKey)
+        req.add_header("X-Pvoutput-SystemId", self.pvoutputSystemId)
+        try:
+            resp = request.urlopen(req)
+        except Exception as e:
+            print("error posting to PVOutput.org: "+str(e))
 
 def main():
     polling = True
@@ -99,6 +210,8 @@ def main():
     pollingCounter.start();
     urlPoster = URLPoster(pollingCounter)
     urlPoster.start();
+    pvoutputPoster = PVOutputPoster(pollingCounter)
+    pvoutputPoster.start();
 
     print ("pausing")
     signal.signal(signal.SIGINT, signal_handler)
@@ -106,11 +219,12 @@ def main():
     print ("stopping threads")
     killEventPulseCounter.set()
     killEventURLPoster.set()
+    killEventPvoutputPoster.set()
     print ("joining threads")
 
     try:
         pollingCounter.join(1);
-        print ("thread exited")
+        print ("threads exited")
     except:
         print ("exception")
 
@@ -121,4 +235,3 @@ def signal_handler(signal, frame):
     print('You pressed Ctrl+C!')
 
 main()
-
